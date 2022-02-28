124d5ead03d0c1da86fba8c159e012f1ccfbd100
Jmorg21 <Jmorg21@gmail.com>
2022-02-27 19:32:12 -0500

    case prompts and var

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+var askPasswordCase = function() {
+  var promptCase = window.prompt('Would you like only lowercase letters, only uppercase letters, or both? Type "lower", "upper", or "both".');
+
+  if (promptCase === "" || promptCase === null) {
+    window.alert('Please type "lower", "upper", or "both".');
+    return askPasswordCase();
+  }
+
+  promptCase = promptCase.toLowerCase();
+
+  if (promptCase === "lower") {
+    passwordCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    chooseNumber();
+
+  if (promptCase === "upper") {
+    passwordCase =["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    chooseNumber(); 
+  }
+
+  if (promptCase === "both") {
+    passwordCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    chooseNumber();
+  }
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
